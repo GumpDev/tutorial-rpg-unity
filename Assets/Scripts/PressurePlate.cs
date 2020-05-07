fbc635e414a5f05b31dd1118ce0ed23e52778849
GumpFlash <gump.flash@gmail.com>
2020-05-07 15:18:35 -0300

    ep2

---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public GameObject camadaPorta;
+    public float tempoParaFechar = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    Coroutine rotina = null;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player" && rotina == null) 
+            rotina = StartCoroutine(abrirPortas());
+    }
+
+    IEnumerator abrirPortas(){
+        camadaPorta.SetActive(false);
+        yield return new WaitForSeconds(tempoParaFechar);
+        camadaPorta.SetActive(true);
+        rotina = null;
+    }
+}
